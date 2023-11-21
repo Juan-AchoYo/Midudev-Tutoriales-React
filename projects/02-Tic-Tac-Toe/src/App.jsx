a793f719e8567b4ebd75aa35691b59ea3cb881d6
JuanValeraDev <juanvaleradev@gmail.com>
2023-11-21 19:27:19 +0100

    He avanzado un poco

--- projects/02-Tic-Tac-Toe/src/App.jsx
+++ projects/02-Tic-Tac-Toe/src/App.jsx
@@ -1,30 +1,35 @@
 import './App.css'
+import {useState} from "react";
 
 const TURNS = {
-    x: 'X',
-    o: 'O'
+    x: '×', o: 'o'
 }
-const Square = ({children, updateBoard, index}) => {
+const Square = ({children, isSelected, updateBoard, index}) => {
+    const className = `square ${isSelected ? 'selected' : '' }`
+    console.log(className)
     return <div className={"square"}>
         {children}
     </div>
 }
 
 export default function App() {
-const board = Array(9).fill(null)
-    return (
-        <main className={"board"}>
-            <h1>Tic-Tac-Toe</h1>
-            <section className={"game"}>
-                {
-                    board.map((_, index) => {
-                        return (
-                            <Square key={index} index={index}>{index}</Square>
-                        )
-                    })
-                }
-            </section>
-        </main>
-    )
+    const [board, setBoard] = useState(Array(9).fill(null))
+    const [turn, setTurn] = useState(TURNS.x)
+    return (<main className={"board"}>
+        <h1>Tic-Tac-Toe</h1>
+        <section className={"game"}>
+            {board.map((_, index) => {
+                return (<Square key={index} index={index}></Square>)
+            })}
+        </section>
+        <section className={"turn"}>
+            <Square isSelected={turn === TURNS.x}>
+                {TURNS.x}
+            </Square>
+            <Square isSelected={turn === TURNS.o}>
+                {TURNS.o}
+            </Square>
+        </section>
+    </main>)
 }
 
